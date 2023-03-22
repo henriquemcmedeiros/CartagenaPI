ab0c866aaf8fad50a565e8817ae199dd13488783
Henrique Medeiros <84346920+henriquemcmedeiros@users.noreply.github.com>
2023-03-22 01:31:40 -0300

    Entrega 22/03 - Parcial
    Listar jogadores e criação do form após iniciar a partida.
    
    Co-Authored-By: Gustavo Guimarães Pardini <104874698+Pardiniii@users.noreply.github.com>

--- Cartagena PI ofc/Form2.Designer.cs
+++ Cartagena PI ofc/Form2.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Cartagena
+﻿namespace Cartagena_PI_ofc
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -31,10 +31,9 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Form2";
         }
 
         #endregion
     }
 }
-
--- Cartagena PI ofc/Form2.cs
+++ Cartagena PI ofc/Form2.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Cartagena
+namespace Cartagena_PI_ofc
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
