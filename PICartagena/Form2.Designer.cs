d6eb6064732cbfef07e4dc80c512461c1faf0b6c
Gustavo Guimarães Pardini <ggpardini@gmail.com>
2023-04-23 12:03:08 -0300

    merge changes

--- PICartagena/Form2.Designer.cs
+++ PICartagena/Form2.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
             this.lblJogadorAtual = new System.Windows.Forms.Label();
             this.lblEsqueleto = new System.Windows.Forms.Label();
             this.lblFaca = new System.Windows.Forms.Label();
@@ -38,86 +39,109 @@
             this.lblTabuleiro = new System.Windows.Forms.Label();
             this.btnJogar = new System.Windows.Forms.Button();
             this.txtCarta = new System.Windows.Forms.TextBox();
-            this.txtPosicaoPirata = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // lblJogadorAtual
             // 
             this.lblJogadorAtual.AutoSize = true;
-            this.lblJogadorAtual.Location = new System.Drawing.Point(55, 51);
+            this.lblJogadorAtual.Location = new System.Drawing.Point(37, 33);
+            this.lblJogadorAtual.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblJogadorAtual.Name = "lblJogadorAtual";
-            this.lblJogadorAtual.Size = new System.Drawing.Size(143, 20);
+            this.lblJogadorAtual.Size = new System.Drawing.Size(97, 13);
             this.lblJogadorAtual.TabIndex = 0;
             this.lblJogadorAtual.Text = "O Jogador atual é: ";
             // 
             // lblEsqueleto
             // 
             this.lblEsqueleto.AutoSize = true;
-            this.lblEsqueleto.Location = new System.Drawing.Point(74, 243);
+            this.lblEsqueleto.BackColor = System.Drawing.Color.Transparent;
+            this.lblEsqueleto.ForeColor = System.Drawing.SystemColors.MenuBar;
+            this.lblEsqueleto.Location = new System.Drawing.Point(117, 366);
+            this.lblEsqueleto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblEsqueleto.Name = "lblEsqueleto";
-            this.lblEsqueleto.Size = new System.Drawing.Size(51, 20);
+            this.lblEsqueleto.Size = new System.Drawing.Size(14, 13);
             this.lblEsqueleto.TabIndex = 1;
-            this.lblEsqueleto.Text = "label1";
+            this.lblEsqueleto.Text = "E";
             // 
             // lblFaca
             // 
             this.lblFaca.AutoSize = true;
-            this.lblFaca.Location = new System.Drawing.Point(146, 243);
+            this.lblFaca.Location = new System.Drawing.Point(245, 366);
+            this.lblFaca.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblFaca.Name = "lblFaca";
-            this.lblFaca.Size = new System.Drawing.Size(51, 20);
+            this.lblFaca.Size = new System.Drawing.Size(13, 13);
             this.lblFaca.TabIndex = 2;
-            this.lblFaca.Text = "label2";
+            this.lblFaca.Text = "F";
+            this.lblFaca.Click += new System.EventHandler(this.lblFaca_Click);
             // 
             // lblGarrafa
             // 
             this.lblGarrafa.AutoSize = true;
-            this.lblGarrafa.Location = new System.Drawing.Point(221, 243);
+            this.lblGarrafa.Location = new System.Drawing.Point(371, 366);
+            this.lblGarrafa.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblGarrafa.Name = "lblGarrafa";
-            this.lblGarrafa.Size = new System.Drawing.Size(51, 20);
+            this.lblGarrafa.Size = new System.Drawing.Size(15, 13);
             this.lblGarrafa.TabIndex = 3;
-            this.lblGarrafa.Text = "label3";
+            this.lblGarrafa.Text = "G";
             // 
             // lblTricornio
             // 
             this.lblTricornio.AutoSize = true;
-            this.lblTricornio.Location = new System.Drawing.Point(300, 243);
+            this.lblTricornio.Location = new System.Drawing.Point(120, 536);
+            this.lblTricornio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTricornio.Name = "lblTricornio";
-            this.lblTricornio.Size = new System.Drawing.Size(51, 20);
+            this.lblTricornio.Size = new System.Drawing.Size(14, 13);
             this.lblTricornio.TabIndex = 4;
-            this.lblTricornio.Text = "label4";
+            this.lblTricornio.Text = "T";
             // 
             // lblChave
             // 
             this.lblChave.AutoSize = true;
-            this.lblChave.Location = new System.Drawing.Point(372, 243);
+            this.lblChave.Location = new System.Drawing.Point(244, 536);
+            this.lblChave.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblChave.Name = "lblChave";
-            this.lblChave.Size = new System.Drawing.Size(51, 20);
+            this.lblChave.Size = new System.Drawing.Size(14, 13);
             this.lblChave.TabIndex = 5;
-            this.lblChave.Text = "label5";
+            this.lblChave.Text = "C";
             // 
             // lblPistola
             // 
             this.lblPistola.AutoSize = true;
-            this.lblPistola.Location = new System.Drawing.Point(447, 243);
+            this.lblPistola.Location = new System.Drawing.Point(371, 536);
+            this.lblPistola.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPistola.Name = "lblPistola";
-            this.lblPistola.Size = new System.Drawing.Size(51, 20);
+            this.lblPistola.Size = new System.Drawing.Size(14, 13);
             this.lblPistola.TabIndex = 6;
-            this.lblPistola.Text = "label6";
+            this.lblPistola.Text = "P";
             // 
             // lblTabuleiro
             // 
             this.lblTabuleiro.AutoSize = true;
-            this.lblTabuleiro.Location = new System.Drawing.Point(584, 51);
+            this.lblTabuleiro.Location = new System.Drawing.Point(890, 189);
+            this.lblTabuleiro.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTabuleiro.Name = "lblTabuleiro";
-            this.lblTabuleiro.Size = new System.Drawing.Size(51, 20);
+            this.lblTabuleiro.Size = new System.Drawing.Size(35, 13);
             this.lblTabuleiro.TabIndex = 7;
             this.lblTabuleiro.Text = "label1";
             // 
             // btnJogar
             // 
-            this.btnJogar.Location = new System.Drawing.Point(748, 233);
+            this.btnJogar.Location = new System.Drawing.Point(420, 46);
+            this.btnJogar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnJogar.Name = "btnJogar";
-            this.btnJogar.Size = new System.Drawing.Size(129, 41);
+            this.btnJogar.Size = new System.Drawing.Size(86, 27);
             this.btnJogar.TabIndex = 8;
             this.btnJogar.Text = "Jogar";
             this.btnJogar.UseVisualStyleBackColor = true;
@@ -125,24 +149,77 @@
             // 
             // txtCarta
             // 
-            this.txtCarta.Location = new System.Drawing.Point(748, 302);
+            this.txtCarta.Location = new System.Drawing.Point(420, 91);
+            this.txtCarta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtCarta.Name = "txtCarta";
-            this.txtCarta.Size = new System.Drawing.Size(129, 26);
+            this.txtCarta.Size = new System.Drawing.Size(87, 20);
             this.txtCarta.TabIndex = 9;
             // 
-            // txtPosicaoPirata
+            // pictureBox1
             // 
-            this.txtPosicaoPirata.Location = new System.Drawing.Point(748, 344);
-            this.txtPosicaoPirata.Name = "txtPosicaoPirata";
-            this.txtPosicaoPirata.Size = new System.Drawing.Size(129, 26);
-            this.txtPosicaoPirata.TabIndex = 10;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 357);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(158, 357);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(286, 357);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(109, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(34, 527);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(159, 527);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(109, 150);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox5.TabIndex = 14;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(285, 527);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(110, 150);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox6.TabIndex = 15;
+            this.pictureBox6.TabStop = false;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1021, 514);
-            this.Controls.Add(this.txtPosicaoPirata);
+            this.ClientSize = new System.Drawing.Size(1214, 716);
             this.Controls.Add(this.txtCarta);
             this.Controls.Add(this.btnJogar);
             this.Controls.Add(this.lblTabuleiro);
@@ -153,8 +230,21 @@
             this.Controls.Add(this.lblFaca);
             this.Controls.Add(this.lblEsqueleto);
             this.Controls.Add(this.lblJogadorAtual);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.Text = "Form2";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -172,6 +262,11 @@
         private System.Windows.Forms.Label lblTabuleiro;
         private System.Windows.Forms.Button btnJogar;
         private System.Windows.Forms.TextBox txtCarta;
-        private System.Windows.Forms.TextBox txtPosicaoPirata;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
     }
 }
